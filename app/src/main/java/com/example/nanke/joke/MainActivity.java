e3d0be25f19f96657930bd253ab5336794192cc0
NanKe <619598856@qq.com>
2017-03-05 19:16:26 +0800

    该用retrofit进行网络请求

--- app/src/main/java/com/example/nanke/joke/MainActivity.java
+++ app/src/main/java/com/example/nanke/joke/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.nanke.joke;
 
 
 import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
 
 import com.cjj.MaterialRefreshLayout;
 import com.cjj.MaterialRefreshListener;
+import com.example.nanke.joke.Api.Api;
 import com.example.nanke.joke.JavaBean.MyJoke;
 
 
@@ -26,29 +28,39 @@ import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class MainActivity extends AppCompatActivity {
+
     private RecyclerView recyclerView;
     private List<MyJoke> jokes = new ArrayList<>();
-    private OkHttpClient okHttpClient;
     private MyAdpter adpter;
     private MaterialRefreshLayout materialRefreshLayout;
-    private final String url = "http://api.laifudao.com/open/xiaohua.json";
-
+    private Api api;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        ReFresh();
+    }
 
+    public void initView() {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_View);
         materialRefreshLayout = (MaterialRefreshLayout) findViewById(R.id.refresh);
-        getData(url);
-        adpter = new MyAdpter(jokes);
-       LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
+        System.out.println("获取的数据长度" + jokes.size());
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+        adpter = new MyAdpter(jokes);
         recyclerView.setAdapter(adpter);
+    }
+
+    public void ReFresh() {
 
         materialRefreshLayout.setMaterialRefreshListener(new MaterialRefreshListener() {
             @Override
@@ -56,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        getData(url);
+                        getData();
                         adpter.refreshData(jokes);
                         materialRefreshLayout.finishRefresh();
                     }
@@ -66,53 +78,26 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void getData(String url) {
+    public void getData() {
 
-        okHttpClient = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(url)
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.laifudao.com/open/")
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
-        Call call = okHttpClient.newCall(request);
-        call.enqueue(new Callback() {
+        api = retrofit.create(Api.class);
+
+        api.getData().enqueue(new retrofit2.Callback<List<MyJoke>>() {
             @Override
-            public void onFailure(Call call, IOException e) {
-                System.out.println("网络请求失败" + e.toString());
+            public void onResponse(retrofit2.Call<List<MyJoke>> call, retrofit2.Response<List<MyJoke>> response) {
+                jokes = response.body();
             }
 
             @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                System.out.println("网络请求成功");
-                if (response.isSuccessful()) {
-                    String json = response.body().string();
-                    jokes = pharseJson(json);
-                }
+            public void onFailure(retrofit2.Call<List<MyJoke>> call, Throwable t) {
+                System.out.println("失败" + t.toString());
             }
         });
 
     }
 
-    public List<MyJoke> pharseJson(final String json) {
-        final List<MyJoke> list1 = new ArrayList<>();
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    JSONArray jsonArray = new JSONArray(json);
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        JSONObject jsonObject = jsonArray.getJSONObject(i);
-                        MyJoke joke = new MyJoke();
-                        joke.setTitle(jsonObject.getString("title"));
-                        joke.setContent(jsonObject.getString("content"));
-                        joke.setUrl(jsonObject.getString("url"));
-                        joke.setPoster(jsonObject.getString("poster"));
-                        System.out.println(jsonObject.getString("title"));
-                        list1.add(joke);
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        return list1;
-    }
 }
